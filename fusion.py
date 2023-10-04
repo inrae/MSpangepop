e22589cf414c70ca477b64e4767957de6838b642
sukanya.denni <sukanya.denni@inrae.fr>
2023-10-04 15:10:25 +0200

    added random sampling of size distribution

--- fusion.py
+++ fusion.py
@@ -1,8 +1,9 @@
 # fusionner le VCF de msprime (position, génotype) et le BED de VISOR
 # pour obtenir un BED avec tous les variants
 # récupérer les variants et les génotypes msprime
-import io
+import io, json
 import pandas as pd
+import numpy as np
 
 def read_vcf(input_file):
 	with open(input_file, "r") as txt:
@@ -25,8 +26,27 @@ def replace_bed_col(input_BED, input_VCF, len_SV):
 	if len(bed) == len(vcf):
 		bed["chr"] = vcf["CHROM"]
 		bed["start"] = vcf["POS"]
+		# piocher dans une distribution de taille de SV --> modéliser sur la distrib humain, etc
 		bed["end"] = vcf["POS"] + len_SV
 	return(bed)
+
+def get_random_len(n):
+	df = pd.read_csv("/home/sukanya/tests/03_results/SV_distrib_human/size_distribINS.tsv", sep="\t")
+	pb = df["pb"].tolist()
+	li = np.random.multinomial(n, pb)
+	print(li)
+	s = []
+	for i in range(len(li)):
+	
+		if li[i] != 0:
+			interval = df["size_interval"].iloc[i]
+			interval = json.loads(interval)
+			# print(interval)
+			s += list(np.random.uniform(interval[0], interval[1], li[i]).round())
+	print(s)
+
+get_random_len(10)
+
 
 # étape 1 : vérifier que les 2 tableaux font la même taille
 # 2 : prendre les positions du VCF + taille = end
