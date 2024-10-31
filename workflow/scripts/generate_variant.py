e99c23bdd9ac23474fe61368abd65be7d1ffc190
lpiat <lucien.piat@inrae.fr>
2024-10-31 15:41:40 +0100

    Work on #26

--- workflow/scripts/generate_variant.py
+++ workflow/scripts/generate_variant.py
@@ -15,26 +15,6 @@ import argparse
 from readfile import read_fasta, read_variant_length_file, read_fai, read_vcf, read_yaml
 from variants_class import *
 
-def select_length(df):
-    """Select a length from the interval based on cumulative probabilities and return an integer length."""
-    rand_val = random.random()
-    row = df[df['cumulative_pb'] >= rand_val].iloc[0]
-    interval = row['size_interval']
-    lower_bound, upper_bound = map(float, interval.strip('[]').split(','))
-    return random.randint(int(lower_bound), int(upper_bound))
-
-def retrieve_reference_sequence(chromosome_dict, chrom, position, length, no_ref=False):
-    """Retrieve the reference sequence from a FASTA file."""
-    if chrom in chromosome_dict:
-        seq_record = chromosome_dict[chrom]
-        start = position - 1 
-        if no_ref:
-            return str(seq_record.seq[start:start + 1])  # Only need one base for insertions
-        end = min(start + length, len(seq_record))
-        return str(seq_record.seq[start:end])
-    else:
-        raise ValueError(f"Chromosome {chrom} not found in FASTA.")
-
 def create_variant(variant_probs, chrom, position, length_files, chrom_lengths, chromosome_dict, samples):
     """Create a variant with a random length based on probabilities and file data, respecting chromosome length."""
     variant_types = list(variant_probs.keys())
@@ -42,41 +22,41 @@ def create_variant(variant_probs, chrom, position, length_files, chrom_lengths, 
     
     selected_variant_type = random.choices(variant_types, weights=probabilities, k=1)[0]
     
-    # Dynamically get the class using globals() and selected_variant_type
+    # Dynamically get the class
     variant_class = globals().get(selected_variant_type)
-    
     if variant_class is None:
         raise ValueError(f"Variant type '{selected_variant_type}' does not correspond to an implemented class.")
     
+    # Instantiate the variant object with relevant parameters
+    # TODO, include no_ref as a variant variable
     if selected_variant_type == 'SNP':
-        variant = variant_class(chrom, position)
+        variant = variant_class(chrom, position, chromosome_dict, chrom_lengths)
     else:
         length_df = length_files[selected_variant_type]
-        length = select_length(length_df)
-        
-        # Ensure the variant length does not exceed the chromosome length
-        max_length = chrom_lengths[chrom] - position
-        if length > max_length:
-            length = max_length
-        if length == 0:
-            length += 1
-            
-        variant = variant_class(chrom, position, length)
+        variant = variant_class(chrom, position, chromosome_dict, chrom_lengths, length_df)
 
-    # Retrieve the reference sequence and attach it to the variant
     no_ref = (selected_variant_type == 'Insertion')
-    variant.reference_seq = retrieve_reference_sequence(chromosome_dict, chrom, position, variant.length, no_ref)
+    variant.reference_seq = variant.retrieve_reference_sequence(no_ref=no_ref)
     
     variant.samples = samples
     variant.compute_alt_seq()
     
-    return variant
+    # Special handling for Translocation
+    if isinstance(variant, Translocation):
+        # Choose a random destination chromosome and position
+        dest_chrom = random.choice(list(chrom_lengths.keys()))
+        dest_position = random.randint(1, chrom_lengths[dest_chrom])
+        variant.set_destination(dest_chrom, dest_position)
+        
+        variant.compute_alt_seq()
 
+    return variant
 
 def main(fai_file, vcf_input_file, vcf_output_file, fasta_file, yaml_file):
     # Load variant probabilities and length files dynamically from YAML input
     variant_probs = read_yaml(yaml_file)
 
+    # Load length distributions for each variant type from files
     length_files = {
         'Deletion': read_variant_length_file('simulation_data/size_distribDEL.tsv'),
         'Insertion': read_variant_length_file('simulation_data/size_distribINS.tsv'),
@@ -88,25 +68,29 @@ def main(fai_file, vcf_input_file, vcf_output_file, fasta_file, yaml_file):
         'ReciprocalTranslocation': read_variant_length_file('simulation_data/size_distribINS.tsv')
     }
 
+    # Load chromosome lengths, VCF data, and chromosome dictionary
     chrom_lengths = read_fai(fai_file)
     vcf_data = read_vcf(vcf_input_file)
     chromosome_dict = read_fasta(fasta_file)
 
+    # Open the output VCF file for writing
     with open(vcf_output_file, 'w') as vcf_output:
-        # Generate and write variants for each row in the VCF
-        for index, random_row in vcf_data.iterrows():
-            chrom = random_row['CHROM']
-            position = random_row['POS']
+        
+        for row in vcf_data.iterrows():
+            chrom = row['CHROM']
+            position = row['POS']
             
-            samples = {col: random_row[col] for col in vcf_data.columns if col.startswith('SAMPLE')}
+            # Extract sample data from VCF row
+            samples = {col: row[col] for col in vcf_data.columns if col.startswith('SAMPLE')}
             
+            # Create a variant instance and generate VCF line output
             variant_instance = create_variant(variant_probs, chrom, position, length_files, chrom_lengths, chromosome_dict, samples)
             vcf_line = variant_instance.vcf_line()
-
+            print(variant_instance.describe())
             vcf_output.write(vcf_line + '\n')
 
 if __name__ == "__main__":
-    # Set up argument parsing with added YAML file path
+    # Set up argument parsing with YAML file path
     parser = argparse.ArgumentParser(description="Generate variants from VCF and other genomic data.")
     parser.add_argument("--fai", help="Path to the FAI file.")
     parser.add_argument("--vcf", help="Path to the input VCF file.")
