e628199526c1ddee61dad265fb635610649bc6bc
lpiat <lucien.piat@inrae.fr>
2024-11-19 14:30:15 +0100

    Add new containers for the job

--- job.sh
+++ job.sh
@@ -2,14 +2,11 @@
 ################################ Slurm options #################################
 ### prepare_calling_jobs
 #SBATCH -J smk_main
-### Max run time "hours:minutes:seconds"
 #SBATCH --time=96:00:00
-#SBATCH --ntasks=1 #nb of processes
-#SBATCH --cpus-per-task=1 # nb of cores for each process(1 process)
-#SBATCH --mem=10G # max of memory (-m) 
-### Requirements nodes/servers (default: 1)
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10G
 #SBATCH --nodes=1
-### Requirements cpu/core/task (default: 1)
 #SBATCH --ntasks-per-node=1
 #SBATCH -o slurm_logs/snakemake.%N.%j.out
 #SBATCH -e slurm_logs/snakemake.%N.%j.err
@@ -17,62 +14,43 @@
 #SBATCH --mail-user=<your.email@here.fr>
 ################################################################################
 
-# Useful information to print
-echo '########################################'
-echo 'Date:' $(date --iso-8601=seconds)
-echo 'User:' $USER
-echo 'Host:' $HOSTNAME
-echo 'Job Name:' $SLURM_JOB_NAME
-echo 'Job ID:' $SLURM_JOB_ID
-echo 'Number of nodes assigned to job:' $SLURM_JOB_NUM_NODES
-echo 'Total number of cores for job (?):' $SLURM_NTASKS
-echo 'Number of requested cores per node:' $SLURM_NTASKS_PER_NODE
-echo 'Nodes assigned to job:' $SLURM_JOB_NODELIST
-echo 'Number of CPUs assigned for each task:' $SLURM_CPUS_PER_TASK
-echo 'Directory:' $(pwd)
-# Detail Information:
-echo 'scontrol show job:'
-scontrol show job $SLURM_JOB_ID
-echo '########################################'
+# Load the Singularity container runtime
+module purge
+module load "containers/singularity/3.9.9" || { echo "Failed to load Singularity module"; exit 1; }
 
-# Function to load modules
-load_modules() {
-    module purge  # Clear any previously loaded modules
+# Define paths to your containers
+PYTHON_CONTAINER="docker://registry.forgemia.inra.fr/pangepop/mspangepop/python:3.9.7"
+SNAKEMAKE_CONTAINER="docker://registry.forgemia.inra.fr/pangepop/mspangepop/snakemake:6.5.1"
 
-    # Loop through each module and load it
-    for module_name in "$@"; do
-        module load "$module_name"
-    done
-}
-
-# Here specify the modules to load and their path
-load_modules "python/3.9.7" "snakemake/6.5.1" 
-
-### variables
+# Bind paths
 SNG_BIND=$(pwd)
 CLUSTER_CONFIG=".config/snakemake_profile/slurm/cluster_config.yml"
 MAX_CORES=10
 PROFILE=".config/snakemake_profile/slurm"
 
-echo 'Starting Snakemake workflow'
+# Print job details
+echo '########################################'
+echo 'Job Details:'
+echo 'Date:' $(date --iso-8601=seconds)
+echo 'User:' $USER
+echo 'Job Name:' $SLURM_JOB_NAME
+echo 'Job ID:' $SLURM_JOB_ID
+echo 'Nodes Allocated:' $SLURM_JOB_NODELIST
+echo 'CPUs Per Task:' $SLURM_CPUS_PER_TASK
+echo 'Working Directory:' $(pwd)
+echo '########################################'
 
 
+# Run Snakemake inside the Snakemake container
 run_snakemake() {
     local snakefile="$1"  # The Snakefile to run
     local option="$2"     # The option for dry run or DAG
 
-    echo "Starting $snakefile..."
+    echo "Starting Snakemake workflow with container..."
 
-    # Execute the Snakemake command with the specified option
-    if [[ "$option" == "dry" ]]; then
-        snakemake -s "$snakefile" --profile $PROFILE -j $MAX_CORES --use-singularity --singularity-args "-B $SNG_BIND" --cluster-config $CLUSTER_CONFIG -n -r
-    elif [[ "$option" == "dag" ]]; then
-        snakemake -s "$snakefile" --profile $PROFILE -j $MAX_CORES --use-singularity --singularity-args "-B $SNG_BIND" --cluster-config $CLUSTER_CONFIG --dag > dag.dot
-        echo "DAG has been generated as dag.png"
-        return
-    else
-        snakemake -s "$snakefile" --profile $PROFILE -j $MAX_CORES --use-singularity --singularity-args "-B $SNG_BIND" --cluster-config $CLUSTER_CONFIG
-    fi
+    singularity exec --bind $SNG_BIND \
+        $SNAKEMAKE_CONTAINER \
+        snakemake -s "$snakefile" --profile $PROFILE -j $MAX_CORES --use-singularity --singularity-args "-B $SNG_BIND" --cluster-config $CLUSTER_CONFIG $option
 
     # Check if the Snakemake command was successful
     if [ $? -eq 0 ]; then
@@ -97,7 +75,20 @@ fi
 workflow="$1"
 option="$2"
 
-# Run the specified Snakefile based on user input
+# Parse options for Snakemake
+case "$option" in
+    dry)
+        snakemake_option="-n -r"
+        ;;
+    dag)
+        snakemake_option="--dag > dag.dot"
+        echo "DAG will be generated as dag.png"
+        ;;
+    *)
+        snakemake_option=""
+        ;;
+esac
+
 case "$workflow" in
     split)
         snakefile="workflow/Snakefile_split.smk"
@@ -112,6 +103,8 @@ case "$workflow" in
         ;;
 esac
 
-# Run the specified Snakefile with the provided option
-run_snakemake "$snakefile" "$option"
+# Run the Snakemake command
+run_snakemake "$snakefile" "$snakemake_option"
+
+# Monitor job queue
 squeue -u $USER
