2eda734eaa66e78d1702e3e9af23c625332ebda5
Lucien Piat <lucien.piat@inrae.fr>
2024-12-04 11:03:46 +0100

    add function to read sv_type.yaml file

--- workflow/scripts/generate_variant_type.py
+++ workflow/scripts/generate_variant_type.py
@@ -1,5 +1,9 @@
 import argparse
 import json
+import random
+import yaml
+
+from readfile import read_variant_length_file, read_yaml
 
 def read_fai_to_dict(fai_path):
     """
@@ -71,14 +75,35 @@ def save_json_file(data, output_path):
         json.dump(data, file, indent=4)
     print(f"JSON data has been saved to {output_path}")
 
+def assign_mutation_type(yaml_data):
+    """
+    Selects a mutation type based on the probabilities in the YAML data.
+    
+    Parameters:
+        yaml_data (dict): The data read from the YAML file, containing the variant percentages.
+        
+    Returns:
+        str: The type of mutation.
+    """
+    # Create a list of mutation types and their corresponding probabilities
+    mutation_types = list(yaml_data.keys())
+    probabilities = list(yaml_data.values())
+    
+    # Randomly select a mutation type based on the given probabilities
+    mutation_type = random.choices(mutation_types, probabilities)[0]
+    
+    return mutation_type
 
-def main(json_file, fai_file, output_json_file):
 
+def main(json_file, fai_file, output_json_file, yaml_file):
     # Read the FAI file into a dictionary
     fai_data = read_fai_to_dict(fai_file)
 
     # Read the JSON file
     parsed_data = read_json_file(json_file)
+
+    # Read the YAML file to get mutation percentages
+    yaml_data = read_yaml(yaml_file)
 
     # Extract the chromosome name from the JSON
     chromosome_name = parsed_data["chromosome"]
@@ -92,14 +117,19 @@ def main(json_file, fai_file, output_json_file):
     else:
         print(f"Chromosome {chromosome_name} not found in FAI data.")
 
+    # Assign mutation types to each mutation
+    for mutation in parsed_data["mutations"]:
+        mutation_type = assign_mutation_type(yaml_data)
+        mutation["mutation_type"] = mutation_type  # Add the mutation type to each mutation
+
     # Save the JSON data to the specified output file
     save_json_file(parsed_data, output_json_file)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Augment the JSON file with variant type")
     parser.add_argument("--json", help="Path to the JSON file.")
     parser.add_argument("--fai", help="Path to the FAI file.")
     parser.add_argument("--output", help="Path to the output file")
+    parser.add_argument("--yaml", help="Path to the YAML file containing variant type percentages")
     args = parser.parse_args()
-    main(args.json, args.fai, args.output)
+    main(args.json, args.fai, args.output, args.yaml)
