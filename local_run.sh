9f0f360397c0100c755bbde2095b74c059b31728
Lucien Piat <lucien.piat@inrae.fr>
2025-02-03 16:56:24 +0100

    Update the localrun based on the asm4pg improvements on it

--- local_run.sh
+++ local_run.sh
@@ -1,65 +1,77 @@
 #!/bin/bash
-# Script to run locally, DO NOT USE AS IS ON A CLUSTER!
+
+## TMP config to run on the CBIB
+#SBATCH --job-name=asm4pg
+#SBATCH --ntasks=25
+#SBATCH --mem=300G
+#SBATCH -o slurm_logs/out_job_%j.out
+#SBATCH -e slurm_logs/err_job_%j.err
+
+# Written by Lucien Piat at INRAe
+# Use this script to run MSpangepop localy or on a single HPC node
+# 03/02/25
 
 SNG_BIND=$(pwd)
 
-# Get the number of CPU cores dynamically
-CORES=$(nproc)
-
 run_snakemake() {
-    local snakefile="$1"  # The Snakefile to run
-    local option="$2"     # The option for dry run or DAG
+    local option="$1"
 
-    echo "Starting $snakefile..."
-
-    # Execute the Snakemake command with the specified option
-    if [[ "$option" == "dry" ]]; then
-        snakemake -s "$snakefile" --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES -n
-    elif [[ "$option" == "dag" ]]; then
-        snakemake -s "$snakefile" --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES --dag > dag.dot
-        echo "DAG has been generated as dag.png"
-        return
-    else
-        snakemake -s "$snakefile" --use-singularity --singularity-args "-B $SNG_BIND" -j $CORES --forceall
-    fi
+    case "$option" in
+        dry)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $(nproc) -n 
+            ;;
+        dag)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $(nproc) --dag > dag.dot
+            if [ $? -eq 0 ]; then
+                echo "MSpangepop -> DAG has been successfully generated as dag.dot"
+            else
+                echo "MSpangepop -> Error: Failed to generate DAG."
+                exit 1
+            fi
+            ;;
+        rulegraph)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $(nproc) --rulegraph > rulegraph.dot
+            if [ $? -eq 0 ]; then
+                echo "MSpangepop -> Rulegraph has been successfully generated as rulegraph.dot"
+            else
+                echo "MSpangepop -> Error: Failed to generate Rulegraph."
+                exit 1
+            fi
+            ;;
+        unlock)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $(nproc) --unlock
+            ;;
+        run)
+            snakemake --use-singularity --singularity-args "-B $SNG_BIND" -j $(nproc) --forceall
+            ;;
+        *)
+            echo "Invalid option: $option"
+            echo "Usage: $0 [dry|run|dag|rulegraph|unlock]"
+            exit 1
+            ;;
+    esac
 
     # Check if the Snakemake command was successful
     if [ $? -eq 0 ]; then
-        echo "$snakefile completed successfully."
+        echo "MSpangepop -> Snakemake workflow completed successfully."
     else
-        echo "Error: $snakefile failed."
+        echo "MSpangepop -> Error: Snakemake workflow execution failed."
         exit 1
     fi
 }
 
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 [split|simulate] [dry|dag|run]"
-    echo "    split - run the split Snakefile"
-    echo "    simulate - run the simulate Snakefile"
+# Verify arguments
+if [ $# -ne 1 ] || [ "$1" == "help" ]; then
+    echo "Use this script to run MSpangepop localy or on a single HPC node"
+    echo ""
+    echo "Usage: $0 [dry|run|dag|rulegraph|unlock]"
     echo "    dry - run the specified Snakefile in dry-run mode"
-    echo "    dag - generate DAG for the specified Snakefile"
-    echo "    run - run the specified Snakefile normally (default)"
+    echo "    run - run the specified Snakefile normally"
+    echo "    dag - generate the directed acyclic graph for the specified Snakefile"
+    echo "    rulegraph - generate the rulegraph for the specified Snakefile"
+    echo "    unlock - Unlock the directory if snakemake crashed"
     exit 1
 fi
 
-# Determine the workflow and option based on the arguments
-workflow="$1"
-option="$2"
-
-# Run the specified Snakefile based on user input
-case "$workflow" in
-    split)
-        snakefile="workflow/Snakefile_split.smk"
-        ;;
-    simulate)
-        snakefile="workflow/Snakefile_simulate.smk"
-        ;;
-    *)
-        echo "Invalid workflow: $workflow"
-        echo "Usage: $0 [split|simulate] [dry|dag|run]"
-        exit 1
-        ;;
-esac
-
-# Run the specified Snakefile with the provided option
-run_snakemake "$snakefile" "$option"
+# Execute the function with the provided option
+run_snakemake "$1"
