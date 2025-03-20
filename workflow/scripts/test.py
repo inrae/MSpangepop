cfe2827fcad7194a9451271f7f69dda40dba396b
Lucien Piat <lucien.piat@inrae.fr>
2025-03-20 15:36:29 +0100

    Start to add some pytest

---
+++ workflow/scripts/test.py
@@ -0,0 +1,22 @@
+import pytest
+import msprime
+from io_handler import MSerror
+from msprime_simulation import (
+    get_chromosome_bounds,
+    create_recombination_map,
+)
+
+def test_get_chromosome_bounds():
+    assert get_chromosome_bounds(1000) == (0, 1000)
+    assert get_chromosome_bounds(500000) == (0, 500000)
+    
+    with pytest.raises(MSerror, match="Chromosome length cannot be negative."):
+        get_chromosome_bounds(-100)
+
+def test_create_recombination_map():
+    chrom_length = 100000
+    recombination_rate = 1e-8
+    recomb_map = create_recombination_map(chrom_length, recombination_rate)
+    
+    assert isinstance(recomb_map, msprime.RateMap)
+    assert recomb_map.num_intervals == 1
